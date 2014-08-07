2f89542395015994df942edd1c0491d925b971d5
Yuriy Kozyrev <yk@multiship.ru>
2014-08-07 16:10:12 +0400

    tests

---
+++ tests/LoadTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Yury Kozyrev <ykmship@yandex-team.ru>
+ */
+
+namespace Kozz\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class LoadTest extends PHPUnit_Framework_TestCase
+{
+  public function testLoad()
+  {
+    $this->assertTrue(class_exists('EMongoCriteria'));
+    $this->assertTrue(class_exists('EMongoCursor'));
+    $this->assertTrue(class_exists('EMongoDB'));
+    $this->assertTrue(class_exists('EMongoDocument'));
+    $this->assertTrue(class_exists('EMongoDocumentBehavior'));
+    $this->assertTrue(class_exists('EMongoEmbeddedDocument'));
+    $this->assertTrue(class_exists('EMongoException'));
+    $this->assertTrue(class_exists('EMongoGridFS'));
+    $this->assertTrue(class_exists('EMongoModifier'));
+    $this->assertTrue(class_exists('EMongoSoftDocument'));
+
+    $this->assertTrue(class_exists('EEmbeddedArraysBehavior'));
+    $this->assertTrue(class_exists('EMongoPartialDocument'));
+    $this->assertTrue(class_exists('EMongoUniqueValidator'));
+
+  }
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author Ura Kozyrev
+ */
+
+require __DIR__ . '/../vendor/yiisoft/yii/framework/yii.php';
+require __DIR__ . '/../vendor/autoload.php';
