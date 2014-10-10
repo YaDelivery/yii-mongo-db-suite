2c0eb1c59980b570d128eed22a9a0dc6fed11393
Yury Kozyrev <ykmship@yandex-team.ru>
2014-10-10 19:33:09 +0400

    embed

--- extra/EEmbeddedArraysBehavior.php
+++ extra/EEmbeddedArraysBehavior.php
@@ -1,17 +1,14 @@
 <?php
+
 /**
- * EEmbeddedArraysBehavior.php
- *
- * PHP version 5.2+
- *
- * @author		Dariusz Górecki <darek.krk@gmail.com>
- * @author		Invenzzia Group, open-source division of CleverIT company http://www.invenzzia.org
- * @copyright	2011 CleverIT http://www.cleverit.com.pl
- * @license		http://www.yiiframework.com/license/ BSD license
- * @version		1.3
- * @category	ext
- * @package		ext.YiiMongoDbSuite
- * @since		v1.0
+ * @author    Ianaré Sévi
+ * @author    Dariusz Górecki <darek.krk@gmail.com>
+ * @author    Invenzzia Group, open-source division of CleverIT company http://www.invenzzia.org
+ * @copyright 2011 CleverIT http://www.cleverit.com.pl
+ * @license   New BSD license
+ * @version   1.3
+ * @category  ext
+ * @package   ext.YiiMongoDbSuite
  */
 
 /**
@@ -19,104 +16,158 @@
  */
 class EEmbeddedArraysBehavior extends EMongoDocumentBehavior
 {
-	/**
-	 * Name of property witch holds array od documents
-	 *
-	 * @var string $arrayPropertyName
-	 * @since v1.0
-	 */
-	public $arrayPropertyName;
+  /**
+   * Name of property witch holds array od documents
+   *
+   * @var string $arrayPropertyName
+   * @since v1.0
+   */
+  public $arrayPropertyName;
 
-	/**
-	 * Class name of doc in array
-	 *
-	 * @var string $arrayDocClassName
-	 * @since v1.0
-	 */
-	public $arrayDocClassName;
+  /**
+   * Class name of doc in array
+   *
+   * @var string $arrayDocClassName
+   * @since v1.0
+   */
+  public $arrayDocClassName;
+  private $_cache;
 
-	private $_cache;
+  /**
+   * This flag shows us if we're connected to an embedded document
+   *
+   * @var boolean $_embeddedOwner
+   */
+  private $_embeddedOwner;
 
-	/**
-	 * @since v1.0
-	 * @see CBehavior::attach()
-	 */
-	public function attach($owner)
-	{
-		parent::attach($owner);
+  public function events()
+  {
+    if (!$this->_embeddedOwner)
+    {
+      return parent::events();
+    }
+    else
+    {
+      // If attached to an embedded document these events are not defined
+      // and would throw an error if attached to
+      $events = parent::events();
+      unset($events['onBeforeSave']);
+      unset($events['onAfterSave']);
+      unset($events['onBeforeDelete']);
+      unset($events['onAfterDelete']);
+      unset($events['onBeforeFind']);
+      unset($events['onAfterFind']);
 
-		// Test if we have correct embding class
-		if(!is_subclass_of($this->arrayDocClassName, 'EMongoEmbeddedDocument'))
-			throw new CException(Yii::t('yii', get_class($testObj).' is not a child class of EMongoEmbeddedDocument!'));
+      return $events;
+    }
+  }
 
-		$this->parseExistingArray();
-	}
+  /**
+   * @since v1.0
+   * @see   CBehavior::attach()
+   */
+  public function attach($owner)
+  {
+    // Test if we have correct embding class
+    if (!is_subclass_of($this->arrayDocClassName, 'EMongoEmbeddedDocument'))
+    {
+      throw new EMongoException(Yii::t(
+          'yii',
+          $this->arrayDocClassName . ' is not a child class of EMongoEmbeddedDocument.'
+        ));
+    }
 
-	/**
-	 * Event: initialize array of embded documents
-	 * @since v1.0
-	 */
-	public function afterEmbeddedDocsInit($event)
-	{
-		$this->parseExistingArray();
-	}
+    $this->_embeddedOwner = !($owner instanceof EMongoDocument);
 
-	/**
-	 * @since v1.0
-	 */
-	private function parseExistingArray()
-	{
-		if(is_array($this->getOwner()->{$this->arrayPropertyName}))
-		{
-			$arrayOfDocs = array();
-			foreach($this->getOwner()->{$this->arrayPropertyName} as $doc)
-			{
-				$obj = new $this->arrayDocClassName;
-				$obj->setAttributes($doc, false);
-				$arrayOfDocs[] = $obj;
-			}
-			$this->getOwner()->{$this->arrayPropertyName} = $arrayOfDocs;
-		}
-	}
+    parent::attach($owner);
 
-	/**
-	 * @since v1.0.2
-	 */
-	public function afterValidate($event)
-	{
-		parent::afterValidate($event);
-		foreach($this->getOwner()->{$this->arrayPropertyName} as $doc)
-		{
-			if(!$doc->validate())
-				$this->getOwner()->addErrors($doc->getErrors());
-		}
-	}
+    $this->parseExistingArray();
+  }
 
-	public function beforeToArray($event)
-	{
-		if(is_array($this->getOwner()->{$this->arrayPropertyName}))
-		{
-			$arrayOfDocs = array();
-			$this->_cache = $this->getOwner()->{$this->arrayPropertyName};
+  /**
+   * Event: initialize array of embded documents
+   *
+   * @since v1.0
+   */
+  public function afterEmbeddedDocsInit($event)
+  {
+    $this->parseExistingArray();
+  }
 
-			foreach($this->_cache as $doc)
-			{
-				$arrayOfDocs[] = $doc->toArray();
-			}
+  /**
+   * @since v1.0
+   */
+  private function parseExistingArray()
+  {
+    if (is_array($this->getOwner()->{$this->arrayPropertyName}))
+    {
+      $arrayOfDocs = array();
+      foreach ($this->getOwner()->{$this->arrayPropertyName} as $doc)
+      {
+        $obj = new $this->arrayDocClassName;
+        $obj->setAttributes($doc, false);
+        $obj->setOwner($this->getOwner());
 
-			$this->getOwner()->{$this->arrayPropertyName} = $arrayOfDocs;
-			return true;
-		}
-		else
-			return false;
-	}
+        // If any EEmbeddedArraysBehavior is attached,
+        // then we should trigger parsing of the newly set
+        // attributes
+        foreach (array_keys($obj->behaviors()) as $name)
+        {
+          $behavior = $obj->asa($name);
+          if ($behavior instanceof EEmbeddedArraysBehavior)
+          {
+            $behavior->parseExistingArray();
+          }
+        }
+        $arrayOfDocs[] = $obj;
+      }
+      $this->getOwner()->{$this->arrayPropertyName} = $arrayOfDocs;
+    }
+  }
 
-	/**
-	 * Event: re-initialize array of embedded documents which where toArray()ized by beforeSave()
-	 */
-	public function afterToArray($event)
-	{
-		$this->getOwner()->{$this->arrayPropertyName} = $this->_cache;
-		$this->_cache = null;
-	}
+  /**
+   * @since v1.0.2
+   */
+  public function afterValidate($event)
+  {
+    parent::afterValidate($event);
+    foreach ($this->getOwner()->{$this->arrayPropertyName} as $doc)
+    {
+      if (!$doc->validate())
+      {
+        $this->getOwner()->addErrors($doc->getErrors());
+      }
+    }
+  }
+
+  public function beforeToArray($event)
+  {
+    if (is_array($this->getOwner()->{$this->arrayPropertyName}))
+    {
+      $arrayOfDocs = array();
+      $this->_cache = $this->getOwner()->{$this->arrayPropertyName};
+
+      foreach ($this->_cache as $doc)
+      {
+        $arrayOfDocs[] = $doc->toArray();
+      }
+
+      $this->getOwner()->{$this->arrayPropertyName} = $arrayOfDocs;
+
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Event: re-initialize array of embedded documents which where toArray()ized by beforeSave()
+   */
+  public function afterToArray($event)
+  {
+    $this->getOwner()->{$this->arrayPropertyName} = $this->_cache;
+    $this->_cache                                 = null;
+  }
 }
--- extra/EMongoUniqueValidator.php
+++ extra/EMongoUniqueValidator.php
@@ -19,23 +19,23 @@
  */
 class EMongoUniqueValidator extends CValidator
 {
-	public $allowEmpty=true;
+  public $allowEmpty=true;
 
-	public function validateAttribute($object, $attribute)
-	{
-		$value = $object->{$attribute};
-		if($this->allowEmpty && ($value === null || $value === ''))
-			return;
+  public function validateAttribute($object, $attribute)
+  {
+    $value = $object->{$attribute};
+    if($this->allowEmpty && ($value === null || $value === ''))
+      return;
 
-		$criteria = new EMongoCriteria;
-		$criteria->{$attribute} = $value;
-		$count = $object->model()->count($criteria);
+    $criteria = new EMongoCriteria;
+    $criteria->{$attribute} = $value;
+    $count = $object->model()->count($criteria);
 
-		if($count !== 0)
-			$this->addError(
-				$object,
-				$attribute,
-				Yii::t('yii', '{attribute} is not unique in DB.')
-			);
-	}
+    if($count !== 0)
+      $this->addError(
+        $object,
+        $attribute,
+        Yii::t('yii', '{attribute} is not unique in DB.')
+      );
+  }
 }
