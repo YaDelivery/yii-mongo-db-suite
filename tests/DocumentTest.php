8ae269cf35f009b9f732b9387c364ab6e51f6d40
Yury Kozyrev <ykmship@yandex-team.ru>
2014-10-11 01:05:21 +0400

    revert to lib-extra

---
+++ tests/DocumentTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author ykmship@yandex-team.ru
+ * Date: 11/10/14
+ */
+
+namespace Kozz\tests;
+
+use PHPUnit_Framework_TestCase;
+
+
+class DocumentTest extends PHPUnit_Framework_TestCase
+{
+
+
+} 
